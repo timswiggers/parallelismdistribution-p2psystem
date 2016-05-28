0069a81cbc6315dd040a6027f5f6505fdccfd311
Tim Swiggers <tim.swiggers@hotmail.com>
2016-05-28 11:14:27 +0200

    health check (WIP)

--- src/peers/network/P2PNetwork.java
+++ src/peers/network/P2PNetwork.java
@@ -1,7 +1,6 @@
 package peers.network;
 
 import discovery.DiscoveryClient;
-import filesystem.FileSystemIndex;
 import io.local.FileAccess;
 import peers.PeerIndex;
 import peers.PeerInfo;
@@ -11,45 +10,98 @@ import peers.selector.PeerSelector;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class P2PNetwork {
     private final PeerIndex peerIndex;
     private final PeerSelector peerSelector;
     private final DiscoveryClient discoveryClient;
 
+    private NetworkState networkState;
+    private Timer networkHealthCheckTimer;
+
+    enum NetworkState{
+        Disconnected,
+        Connecting,
+        Connected
+    }
+
     public P2PNetwork(FileAccess fileAccess, DiscoveryClient discoveryClient) {
         this.peerIndex = new PeerIndex(fileAccess);
         this.discoveryClient = discoveryClient;
         this.peerSelector = new LeastAmountOfFilesSelector(this);
+        this.networkState  = NetworkState.Disconnected;
+        this.networkHealthCheckTimer = new Timer();
     }
 
     public void connect(){
+        if (networkState != NetworkState.Disconnected) {
+            return;
+        }
+        networkState = NetworkState.Connecting;
+
         try{
             connectSafe();
             runPeriodicHealthChecks();
+            networkState = NetworkState.Connected;
         } catch(IOException e){
             e.printStackTrace();
+            networkState = NetworkState.Disconnected;
         }
+    }
+
+    public void disconnect(){
+        // TODO:
+        // Close own health check socket
+        networkHealthCheckTimer.purge();
+        networkState = NetworkState.Disconnected;
     }
 
     private void connectSafe() throws IOException {
 
-        Collection<PeerInfo> peers = peerIndex.list();
-        Collection<PeerInfo> healthyPeers = doHealthCheckOn(peers);
+        boolean networkIsHealthy = doHealthCheck();
+        if (!networkIsHealthy) {
+            resupplyPeers();
+        }
 
         // TODO:
         // 1 Load in known peers
         // 2 Determine if the network is available enough
         // - Do a health check on known peers
         // - If the network is not healthy enough, contact the discovery service for more peers
-        resupplyPeers();
+        // 3 Start own health check socket
+    }
+
+    private boolean doHealthCheck(){
+
+        Collection<PeerInfo> peers = peerIndex.list();
+        Collection<PeerInfo> healthyPeers = doHealthCheckOn(peers);
+
+        updateIndexOnHealthCheck(peers, healthyPeers);
+
+        return healthyPeers.size() > 5;
     }
 
     private Collection<PeerInfo> doHealthCheckOn(Collection<PeerInfo> peers) {
-        
+
     }
 
     private void runPeriodicHealthChecks() {
+        final long fiveMinutes = 5 * 60 * 1000;
+
+        networkHealthCheckTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                boolean networkIsHealthy = doHealthCheck();
+                if (!networkIsHealthy) {
+                    resupplyPeers();
+                }
+            }
+        }, fiveMinutes, fiveMinutes);
+    }
+
+    private void updateIndexOnHealthCheck(Collection<PeerInfo> originalPeers, Collection<PeerInfo> healthyPeers){
 
     }
 
