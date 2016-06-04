c519c2b4f7f683abd4788b8147894bd213d01e3f
Tim Swiggers <tim.swiggers@hotmail.com>
2016-06-04 22:09:07 +0200

    Fixed key and initvector generators

--- src/encryption/Encryptor.java
+++ src/encryption/Encryptor.java
@@ -1,10 +1,10 @@
 package encryption;
 
+import org.apache.commons.lang3.RandomUtils;
+
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
@@ -12,28 +12,26 @@ import java.security.SecureRandom;
 // Credits: http://stackoverflow.com/questions/15554296/simple-java-aes-encrypt-decrypt-example
 
 public class Encryptor {
-    private final static String InitVector = "";
-
     private static final String Encoding = "UTF-8";
     private static final String CipherTransformation = "AES/CBC/PKCS5PADDING";
     private static final String CipherKeySpec = "AES";
 
     private static final SecureRandom random = new SecureRandom();
 
-    public static String generateKey() throws NoSuchProviderException, NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(CipherKeySpec);
-        keyGenerator.init(128);
-        return keyGenerator.generateKey().toString();
+    private Encryptor(){ }
+
+    public static byte[] generateKey() throws NoSuchProviderException, NoSuchAlgorithmException {
+        return RandomUtils.nextBytes(16); // Must be 16!
     }
 
-    public static String generateInitVector(){
-        return new BigInteger(130, random).toString(32);
+    public static byte[] generateInitVector(){
+        return RandomUtils.nextBytes(16); // Must be 16!
     }
 
-    public static byte[] encrypt(String key, byte[] value) {
+    public static byte[] encrypt(byte[] key, byte[] initVector, byte[] value) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(InitVector.getBytes(Encoding));
-            SecretKeySpec sKeySpec = new SecretKeySpec(key.getBytes(Encoding), CipherKeySpec);
+            IvParameterSpec iv = new IvParameterSpec(initVector);
+            SecretKeySpec sKeySpec = new SecretKeySpec(key, CipherKeySpec);
 
             Cipher cipher = Cipher.getInstance(CipherTransformation);
             cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, iv);
@@ -46,10 +44,10 @@ public class Encryptor {
         return null;
     }
 
-    public static byte[] decrypt(String key, byte[] encrypted) {
+    public static byte[] decrypt(byte[] key, byte[]initVector, byte[] encrypted) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(InitVector.getBytes(Encoding));
-            SecretKeySpec sKeySpec = new SecretKeySpec(key.getBytes(Encoding), CipherKeySpec);
+            IvParameterSpec iv = new IvParameterSpec(initVector);
+            SecretKeySpec sKeySpec = new SecretKeySpec(key, CipherKeySpec);
 
             Cipher cipher = Cipher.getInstance(CipherTransformation);
             cipher.init(Cipher.DECRYPT_MODE, sKeySpec, iv);
